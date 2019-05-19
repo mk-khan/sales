a5464aa49435f0d81ba82106be6440a51b623ab8
bi od <binodnp@outlook.com>
2019-05-19 23:47:18 +0545

    Fixed a bug in SQL update.

--- db/SQL Server/2.1.update/src/01.types-domains-tables-and-constraints/tables-and-constraints.sql
+++ db/SQL Server/2.1.update/src/01.types-domains-tables-and-constraints/tables-and-constraints.sql
@@ -4,13 +4,15 @@ ALTER COLUMN sales_id bigint NULL;
 ALTER TABLE sales.returns
 ALTER COLUMN transaction_master_id bigint NULL;
 
-ALTER TABLE sales.orders 
-ADD [priority] national character varying(24);
+IF COL_LENGTH('sales.orders', 'priority') IS NULL
+BEGIN
+	ALTER TABLE sales.orders 
+	ADD [priority] national character varying(24);
+END;
 
 IF COL_LENGTH('sales.customerwise_selling_prices', 'is_taxable') IS NULL
 BEGIN
 	ALTER TABLE sales.customerwise_selling_prices
 	ADD is_taxable bit NOT NULL DEFAULT(0);
 END;
-
 
